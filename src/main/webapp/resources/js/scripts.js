5eeaccf3a81d93680ad529ec76909d84045d23bc
Filipe <libertadores81@gmail.com>
2016-12-10 11:53:00 -0200

    O que faltava

---
+++ src/main/webapp/resources/js/scripts.js
@@ -0,0 +1,13 @@
+$("#a").hide();
+$('#').fadeIn(20000).html(data);
+setTimeout(10000);
+
+$(document).ready(function () {
+    /*    $("#hide").click(function(){
+     $("#a").hide();
+     });
+     $("#show").click(function(){
+     $("#a").show();
+     });*/
+
+});
